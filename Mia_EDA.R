acb33c0e0899bbfa1d7cd8cc1b0ddded3fbe65e7
Mia Childers <mchi8009@uni.sydney.edu.au>
2024-07-22 23:25:09 +1000

    pngs and gif

--- Mia_EDA.R
+++ Mia_EDA.R
@@ -6,11 +6,38 @@ life_expectancy$Entity[life_expectancy$Entity == "Congo"] <- "Republic of Congo"
 life_expectancy_2020 = subset(life_expectancy, Year=='2020')
 
 library(tidyverse)
-library(gganimate)
+library(magick)
 
 world_map = subset(map_data("world"), region != "Antarctica")
-ggplot() +
+
+i = 1950
+while (i <= max(as.numeric(life_expectancy$Year))) {
+  p = ggplot() +
+    geom_polygon(data = world_map, aes(x = long, y = lat, group = group),
+                 fill = "grey", alpha = 0.3) +
+    geom_map(map = world_map, data = subset(life_expectancy, Year==toString(i)), aes(map_id=Entity, fill=Life_Expectancy)) +
+    scale_fill_gradient(low = "#ee4c02", high = "#fcf7ec", name = "Life Expectancy",
+                        limits = c(min(life_expectancy$Life_Expectancy), max(life_expectancy$Life_Expectancy))) +
+    theme_void() + coord_fixed(1.2)
+  filename = paste("plots/",toString(i),".png",sep="")
+  ggsave(filename,p)
+  i = i+1
+}
+
+png_files <- list.files("plots",
+                        pattern = "\\.png$",
+                        recursive = FALSE,
+                        all.files = FALSE,
+                        full.names = TRUE)
+png_files %>%
+  map(image_read) %>% # reads each path file
+  image_join() %>% # joins image
+  image_animate(fps = 5) %>% # animates
+  image_write("All_plots.gif")
+
+'ggplot() +
   geom_polygon(data = world_map, aes(x = long, y = lat, group = group),
                fill = "grey", alpha = 0.3) +
   geom_map(map = world_map, data = life_expectancy_2020, aes(map_id=Entity, fill=Life_Expectancy)) +
   scale_fill_gradient(low = "#cc4c02", high = "#fff7bc", name = "Life Expectancy") + theme_void() + coord_fixed(1.2)
+'
